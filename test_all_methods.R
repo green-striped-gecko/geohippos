7c65636cd8a8a7e8592a0bc577a46e096dbe4686
iwalcott <isobel.walcott@canberra.edu.au>
2023-11-26 21:29:25 +1100

    added Parsapply verion

--- test_all_methods.R
+++ test_all_methods.R
@@ -10,6 +10,7 @@ library(geohippos)
 library(dartR)
 library(parallel)
 library(tictoc)
+library(future)
 
 ###prep data for all methods
 tr_03 <- read.csv("ss100df.csv")
@@ -18,32 +19,33 @@ tr_03
 runcode <- "tr_03"
 
 df  <- tr_03
-df$filename <- str_replace(df$filename, "c:/temp", "/data/scratch/isobel/vcf")
+#df$filename <- str_replace(df$filename, "c:/temp", "/data/scratch/isobel/vcf")
+df$filename <- str_replace(df$filename, "runnum:", "runnum")
 
 ##generate gls files for all
 setwd("~/R/geohippos")
+
+##MCLAPPLY gls file generation####
 df$gls <- mclapply(1:nrow(df), function(x) {
   out <- gl.read.vcf(df$filename[[x]])
   return(out)
 }, mc.cores=20)
 
-
+##generate gls files - non-parallel######
+for (i in 1:nrow(df)) {
+  df$gls[[i]] <- gl.read.vcf(df$filename[[i]])
+}
 
 ##generate loci numbers for all
 for (i in 1:nrow(df)) {
   df$loci[[i]] <- nLoc(df$gls[[i]])
 }
 
-
-
 #Separate gls and loci columns
 dfgls <- df %>% select(runnumb, gls, loci)
 
-
 ####specify settings for each method:
 df.cross <- df[1:10]
-
-
 
 #check os to find correct binaries
 os <- tolower(Sys.info()['sysname'])
@@ -52,12 +54,11 @@ if (os=="darwin") os <- "mac"
 L <- 5e8 #total length of chromosome (for sfs methods)
 mu <- 1e-8  #mutation rate
 
+################################################################################
+#============             SETUP SETTING COMBINATIONS              =============#
+################################################################################
 
-
-#===================================#
-#           Epos testing            #
-#===================================#
-
+#====== EPOS =========#
 #Define settings to test
 ep.settings <- expand_grid(minbin = 1:2, greedy = c("", " -E 2", " -E 5"), .name_repair = "minimal")
 
@@ -67,6 +68,28 @@ test.epos <- as_tibble(crossing(df.cross, ep.settings))
 #Read gls and loci information back in
 test.epos <- left_join(test.epos, dfgls, by = "runnumb")
 
+#====== STAIRWAY =======#
+#Define settings to test
+sw.settings <- expand_grid(minbin = c(1:2), breakpoints = c(4, 6), .name_repair = "minimal")
+
+#Generate all combinations of settings
+test.sw <- as_tibble(crossing(df.cross, sw.settings))
+
+#Read gls and loci information back in
+test.sw <- left_join(test.sw, dfgls, by = "runnumb")
+
+#===== GONE ==========#
+test.g <- df
+
+
+################################################################################
+#============             RUN METHODS WITH MCLAPPLY               =============#
+################################################################################
+
+
+#===================================#
+#           Epos testing            #
+#===================================#
 #Run epos (select for background job)
 library(geohippos)
 library(dartR)
@@ -80,21 +103,9 @@ test.epos$eposout  <- mclapply(1:nrow(test.epos), function(x) {
 
 colnames(test.epos[ncol(test.epos)]) <- paste0("eposout_", runcode)
 
-
-
 #===================================#
 #      StairwayPlot testing         #
 #===================================#
-
-#Define settings to test
-sw.settings <- expand_grid(minbin = c(1:3), breakpoints = c(4, 5, 8), .name_repair = "minimal")
-
-
-#Generate all combinations of settings
-test.sw <- as_tibble(crossing(df.cross, sw.settings))
-
-#Read gls and loci information back in
-test.sw <- left_join(test.sw, dfgls, by = "runnumb")
 
 all.test.sw <- test.sw
 
@@ -106,13 +117,9 @@ test.sw$stairway <- mclapply(1:nrow(test.sw), function(x) {
   return(out)
 }, mc.cores = 10)
 
-
-
 #===================================#
 #           GONE testing            #
 #===================================#
-
-test.g <- df
 
 library(geohippos)
 library(dartR)
@@ -129,5 +136,64 @@ test.g$GONE_00 <- mclapply(1:nrow(test.g), function(x) {
   return(out)
 }, mc.cores = 10)
 
+
+################################################################################
+#==============       RUN METHODS WITH PARSAPPLY                  =============#
+################################################################################
+
+library(doParallel)
+cl <- makeCluster(6) 
+ret <- clusterEvalQ(cl, library("geohippos"))
+ret <- clusterEvalQ(cl, library("future"))
+ret <- clusterExport(cl, varlist = c("test.sw","L","mu")) 
+
+
+#============= EPOS ================#
+epos_sims <- parSapply(cl, 1:nrow(test.epos), function(x) {
+  
+  #load data from fire.  
+  res_epos <- NULL
+  #epos  
+  res_epos <- gl.epos(test.epos$gls[[x]], epos.path = paste0("./binaries/epos/",os),l = L, u=mu, boot=20, minbinsize = test.epos$minbin[[x]], other.options = test.epos$greedy[[x]])
+
+  #save all three in one output
+  out <- list(epos=res_epos)
+  saveRDS(out, file =  file.path("C:/Users/Isobel/Desktop/Honours/RDS_files/", paste0("Epos_", runcode, test.epos$runnumb[x],".rds"))) 
+  return(TRUE)
+})
+
+stopCluster(cl)
+
+
+
+stair_sims <- parSapply(cl, 1:nrow(test.sw), function(x) {
+  
+  #load data from fire.  
+  res_stair <- NULL
+
+  res_stair <- gl.stairway2(test.sw$gls[[x]], verbose = T,stairway.path="./binaries/stairways/", mu = mu, gentime = 1, run=TRUE, nreps = 30, parallel=1, L=L, minbinsize = test.sw$minbin[[x]], cleanup = T, nrand = test.sw$breakpoints[[x]])
+  
+  #save all three in one output
+  out <- res_stair
+  #saveRDS(out, file =  file.path("~/RDS_files/", paste0("Stairway_", runcode, test.sw$runnumb[x],".rds")))
+  return(TRUE)
+})
+
+stopCluster(cl)
+
+gone_sims <- parSapply(cl, 1:nrow(test.g), function(x) {
+  
+  #load data from fire.  
+  res_gone <- NULL
+  
+  res_gone <- gl.gone(test.g$gls[[x]],gone.path = paste0("./binaries/gone/",os), )
+  
+  #save all three in one output
+  out <- list(gone=res_gone)
+  saveRDS(out, file =  file.path("C:/Users/Isobel/Desktop/Honours/RDS_files/", paste0("GONE_", runcode, test.sw$runnumb[x],".rds")))
+  return(TRUE)
+})
+
+stopCluster(cl)
 
 
